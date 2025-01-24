82baaa2fd89a6b303010f4a5e3294f4e2e181254
Moses-93 <moieseenko2001@gmail.com>
2025-01-24 16:37:48 +0200

    add new endpoints to create, receive, and update business information

---
+++ api/v1/endpoints/business_info.py
@@ -0,0 +1,70 @@
+import logging
+from fastapi import APIRouter, Depends, status
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from typing import List
+
+from core.dependencies import get_db, verify_user
+from decorators.permissions import requires_role
+from schemas import business_info
+from db.models import BusinessInfo, User
+from db.crud import crud
+from utils.validators import ensure_resource_exists
+
+
+router = APIRouter(prefix="/business-info", tags=["business-info"])
+logger = logging.getLogger(__name__)
+
+
+@router.get("/", response_model=List[business_info.BusinessInfoResponse])
+@requires_role(["admin", "user"])
+async def get_business_info(
+    user: User = Depends(verify_user), db: AsyncSession = Depends(get_db)
+):
+    result = await crud.read(model=BusinessInfo, session=db)
+    ensure_resource_exists(result)
+    return result
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
+@requires_role(["admin"])
+async def create_business_info(
+    business_info: business_info.BusinessInfoCreate,
+    db: AsyncSession = Depends(get_db),
+    user: User = Depends(verify_user),
+):
+    logger.info(f"Business info: {business_info}")
+    result = await crud.create(
+        model=BusinessInfo,
+        session=db,
+        name=business_info.name,
+        address=business_info.address,
+        phone=business_info.phone,
+        working_hours=business_info.working_hours,
+        google_maps_url=business_info.google_maps_url,
+        description=business_info.description,
+    )
+    ensure_resource_exists(
+        result, status_code=400, message="Failed to create business info"
+    )
+    return result
+
+
+@router.patch("/{name}/", status_code=status.HTTP_204_NO_CONTENT)
+@requires_role(["admin"])
+async def update_business_info(
+    name: str,
+    business_info: business_info.BusinessInfoUpdate,
+    db: AsyncSession = Depends(get_db),
+    user: User = Depends(verify_user),
+):
+    logger.info(f"Update business info:{name}")
+    update_data = business_info.model_dump(exclude_unset=True)
+
+    result = await crud.update(
+        model=BusinessInfo,
+        session=db,
+        expressions=(BusinessInfo.name == name),
+        **update_data,
+    )
+    ensure_resource_exists(result)
