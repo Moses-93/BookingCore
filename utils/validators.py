68963d8a0100be163c0b8d2adc34341037501ebf
Moses-93 <moieseenko2001@gmail.com>
2025-01-29 17:56:05 +0200

    add a function to check the number of masters

--- utils/validators.py
+++ utils/validators.py
@@ -1,4 +1,6 @@
+from typing import Any, Dict
 from fastapi import HTTPException
+from db.models import User
 
 
 def ensure_resource_exists(
@@ -7,3 +9,24 @@ def ensure_resource_exists(
     if not data:
         raise HTTPException(status_code=status_code, detail=message)
     return data
+
+
+def check_number_masters(user: User) -> Dict[str, Any]:
+    """
+    Перевіряє кількість майстрів у користувача та повертає їх.
+
+    :param user: Об'єкт користувача з відношенням до майстрів
+    :return: Словник з кількістю майстрів та їх списком
+    """
+    masters = user.masters
+    if len(masters) > 1:
+        raise HTTPException(
+            status_code=409,
+            detail={
+                "count": len(masters),
+                "masters": [
+                    {"id": master.id, "name": master.name} for master in masters
+                ],
+            },
+        )
+    return masters[0]
