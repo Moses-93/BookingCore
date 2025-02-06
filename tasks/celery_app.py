17e9b56b49786252105d90479481964899547fa6
Moses-93 <moieseenko2001@gmail.com>
2025-02-06 16:58:08 +0200

    configure celery

--- tasks/celery_app.py
+++ tasks/celery_app.py
@@ -2,11 +2,21 @@ from celery import Celery
 from core.config import settings
 
 
-celery_app = Celery("tasks", broker=settings.redis_url, include=["tasks.reminders"])
+celery_app = Celery(
+    "tasks",
+    broker=settings.redis_url,
+    include=["tasks.reminders", "tasks.deactivate_bookings", "tasks.deactivate_dates"],
+)
 
 celery_app.conf.update(
     timezone="UTC",
-    task_routes={"tasks.reminders.send_reminder": {"queue": "reminders"}},
+    task_routes={
+        "tasks.reminders.send_reminder": {"queue": "reminders"},
+        "tasks.deactivate_bookings.deactivate_booking": {
+            "queue": "deactivate_bookings"
+        },
+        "tasks.deactivate_dates.deactivate_date": {"queue": "deactivate_dates"},
+    },
 )
 
 celery_app.autodiscover_tasks(["tasks"])
