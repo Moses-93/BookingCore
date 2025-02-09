d51c460e2f71e9293b9ac5ac7488212ccc54829b
Moses-93 <moieseenko2001@gmail.com>
2025-02-09 17:23:35 +0200

    update command

--- scripts/start_celery.sh
+++ scripts/start_celery.sh
@@ -1,1 +1,1 @@
-celery -A tasks.celery_app worker --loglevel=info --concurrency=4 -Q "reminders,deactivate_bookings,deactivate_dates"
+celery -A tasks.celery_app worker --loglevel=info --concurrency=4 -Q "reminders,deactivate_bookings,deactivate_dates,deactivate_times"
