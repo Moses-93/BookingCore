b598e4bb7a6fc5b01ec8887202979017e106c395
Moses-93 <moieseenko2001@gmail.com>
2025-02-22 18:24:56 +0200

    update booking. move business logic to service

--- api/v1/endpoints/booking.py
+++ api/v1/endpoints/booking.py
@@ -2,50 +2,38 @@ import logging
 from fastapi import APIRouter, Depends, Query, status
 from sqlalchemy.ext.asyncio import AsyncSession
 from typing import List
-
 from core.dependencies import get_db, verify_user
 from decorators.permissions import requires_role
 from schemas import booking
-from db.models import Booking, Master, User
+from db.models import Booking, User
 from db.crud import crud
+from services.booking import booking_service
 from utils.validators import ensure_resource_exists, check_number_masters
-from tasks.reminders import schedule_reminder
 
 
 router = APIRouter(prefix="/bookings", tags=["bookings"])
 logger = logging.getLogger(__name__)
 
 
-@router.get(
-    "/", response_model=List[booking.BookingResponse], status_code=status.HTTP_200_OK
-)
-@requires_role(["master", "user", "admin"])
+@router.get("/", status_code=status.HTTP_200_OK)
+@requires_role(["master", "client", "admin"])
 async def get_bookings(
-    active: bool | None = Query(None),
+    is_active: bool | None = Query(None),
     limit: int = Query(default=5, ge=1, le=50),
-    offset: int = Query(default=0, le=0),
+    offset: int = Query(default=0),
     user: User = Depends(verify_user),
     db: AsyncSession = Depends(get_db),
 ):
-    filters = {"active": active} if active is not None else {}
+    filters = {"is_active": is_active} if is_active is not None else {}
     if user.role == "master":
-        result = await crud.read(model=Master, session=db, user_id=user.id)
-        master = result.unique().scalar_one_or_none()
-        filters["master_id"] = master.id
-    elif user.role == "user":
-        filters["user_id"] = user.id
-
-    result = await crud.read(
-        model=Booking, session=db, limit=limit, offset=offset, **filters
-    )
-
-    bookings = result.unique().scalars().all()
-    ensure_resource_exists(bookings)
+        filters["master_id"] = user.id
+    filters["user_id"] = user.id
+    bookings = await booking_service.get_bookings(db, filters, offset, limit)
     return bookings
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
-@requires_role(["admin", "user"])
+@requires_role(["master", "user"])
 async def create_booking(
     booking: booking.BookingCreate,
     db: AsyncSession = Depends(get_db),
@@ -55,32 +43,17 @@ async def create_booking(
     if not master_id:
         master = check_number_masters(user)
         master_id = master.id
-    result = await crud.create(
-        model=Booking,
-        session=db,
-        user_id=user.id,
-        service_id=booking.service_id,
-        date_id=booking.date_id,
-        time_id=booking.time_id,
-        master_id=master_id,
-        reminder_time=booking.reminder_time,
-    )
-    ensure_resource_exists(result, status_code=400, message="Failed to create booking")
-    if booking.reminder_time:
-        schedule_reminder(
-            user.chat_id,
-            booking.service,
-            booking.date,
-            booking.time,
-            booking.reminder_time,
-        )
-    return result
+
+    new_booking = await booking_service.create_booking(db, booking, user, master_id)
+    return new_booking
 
 
-@router.patch("/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
+@router.patch("/{booking_id}", status_code=status.HTTP_204_NO_CONTENT)
 @requires_role(["user"])
 async def deactivate_book(
-    book_id: int, user: User = Depends(verify_user), db: AsyncSession = Depends(get_db)
+    booking_id: int,
+    user: User = Depends(verify_user),
+    db: AsyncSession = Depends(get_db),
 ):
     result = await crud.update(
         model=Booking, session=db, expressions=(Booking.id == book_id,), active=False
