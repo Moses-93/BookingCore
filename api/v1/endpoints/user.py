1a59d4fc8599f024d6ba867c888326645a80c757
Moses-93 <moieseenko2001@gmail.com>
2025-02-22 18:27:17 +0200

    update user. move business logic to service

--- api/v1/endpoints/user.py
+++ api/v1/endpoints/user.py
@@ -1,51 +1,28 @@
 import logging
-
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import insert
-
-from typing import List
-
 from core.dependencies import get_db, verify_user
 from decorators.permissions import requires_role
-from schemas.user import UserCreate, UserResponse
-from db.models import User, user_master_association
-from db.crud import crud
-from utils.validators import ensure_resource_exists
+from schemas.user import UserCreate, UserResponse, MasterLinkRequest
+from db.models import User
+from services.user import UserService
 
 router = APIRouter(prefix="/users", tags=["users"])
 logger = logging.getLogger(__name__)
 
 
-@router.get("/", response_model=List[UserResponse], status_code=status.HTTP_200_OK)
-@requires_role(["admin"])
-async def get_user(
-    user: User = Depends(verify_user), db: AsyncSession = Depends(get_db)
-):
-    result = await crud.read(model=User, session=db)
-    users = result.unique().scalars().all()
-    ensure_resource_exists(users)
-    return users
+@router.get("/", response_model=UserResponse, status_code=status.HTTP_200_OK)
+@requires_role(["admin", "master", "client"])
+async def get_user(user: User = Depends(verify_user)):
+    return user
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_user(
-    user: UserCreate,
+    user_data: UserCreate,
     db: AsyncSession = Depends(get_db),
 ):
-    user_data = user.model_dump(exclude={"master_id"})
-    new_user = await crud.create(model=User, session=db, **user_data)
-    logger.info(f"New user: {new_user}")
-    ensure_resource_exists(
-        new_user, status_code=400, message="Invalid data for user creation"
-    )
-    stmt = insert(user_master_association).values(
-        user_id=new_user.id, master_id=user.master_id
-    )
-    await db.execute(stmt)
-    await db.commit()
-    logger.info(f"User {new_user.name} created successfully")
-
+    new_user = await UserService(db).create_user(user_data)
     return new_user
 
 
@@ -61,19 +38,7 @@ async def link_master_to_user(
 @router.delete("/", status_code=status.HTTP_204_NO_CONTENT)
 @requires_role(["client", "master"])
 async def delete_user(
-    user_id: int,
     user: User = Depends(verify_user),
     db: AsyncSession = Depends(get_db),
 ):
-    if user_id == user.id:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Users cannot delete themselves",
-        )
-
-    result = await crud.delete(
-        model=User,
-        session=db,
-        id=user_id,
-    )
-    ensure_resource_exists(result)
+    await UserService(db).delete_user(user.id)
