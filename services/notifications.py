d5656fc046fa89d37d1e664d94da541aa9ce04dd
Moses-93 <moieseenko2001@gmail.com>
2025-03-03 16:56:44 +0200

    Change “requests” to httpx for async requests. Create the class NotificationService

--- services/notifications.py
+++ services/notifications.py
@@ -1,21 +1,27 @@
 import logging
-import requests
+from httpx import RequestError
 from core.config import settings
+from .http_client import HTTPClient
 
 
 logger = logging.getLogger(__name__)
 
-TOKEN = settings.telegram_sender_token
-URL = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+class NotificationService:
+
+    token = settings.telegram_main_token
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
+
+    async def send_message(self, chat_id: int, message: str):
+
+        params = {"chat_id": chat_id, "text": message}
+        async with HTTPClient() as client:
+            try:
+                response = await client.get(self.url, params=params)
+                if response.status_code != 200:
+                    logger.error(f"Помилка {response.status_code}: {response.text}")
+            except RequestError as e:
+                logger.error(f"Помилка при відправленні повідомлення: {e}")
 
 
-def send_message(chat_id: int, message: str):
-
-    params = {"chat_id": chat_id, "text": message}
-
-    try:
-        response = requests.get(URL, params=params, timeout=5)
-        if response.status_code != 200:
-            logger.error(f"Помилка {response.status_code}: {response.text}")
-    except requests.exceptions.RequestException as e:
-        logger.error(f"Помилка при відправленні повідомлення: {e}")
+notification_service = NotificationService()
