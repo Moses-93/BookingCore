b9af51ec950a98917ec8d5764d17188c6fcc19fd
Moses-93 <moieseenko2001@gmail.com>
2025-03-06 16:39:42 +0200

    Update user

--- api/v1/endpoints/user.py
+++ api/v1/endpoints/user.py
@@ -1,7 +1,7 @@
 import logging
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
-from core.dependencies import get_db, verify_user
+from core.dependencies import get_db, get_current_user
 from decorators.permissions import requires_role
 from schemas.user import UserCreate, UserResponse, MasterLinkRequest
 from db.models.user import User
@@ -30,7 +30,7 @@ async def create_user(
 @router.post("/masters/", status_code=status.HTTP_204_NO_CONTENT)
 async def link_master_to_user(
     master: MasterLinkRequest,
-    user: User = Depends(verify_user),
+    user: User = Depends(get_current_user),
     db: AsyncSession = Depends(get_db),
 ):
     await UserService(db).add_master_to_user(user.id, master.master_chat_id)
@@ -39,7 +39,7 @@ async def link_master_to_user(
 @router.delete("/", status_code=status.HTTP_204_NO_CONTENT)
 @requires_role(["client", "master"])
 async def delete_user(
-    user: User = Depends(verify_user),
+    user: User = Depends(get_current_user),
     db: AsyncSession = Depends(get_db),
 ):
     await UserService(db).delete_user(user.id)
