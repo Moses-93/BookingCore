11fdbd0827b6369aec4e4e8cf977ada3873ab01b
Moses-93 <moieseenko2001@gmail.com>
2025-03-06 16:33:50 +0200

    Update the business information module. Move business logic to a separate module

--- api/v1/endpoints/business_info.py
+++ api/v1/endpoints/business_info.py
@@ -1,77 +1,65 @@
 import logging
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, status, Query
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from typing import List
 
-from core.dependencies import get_db, verify_user
+from core.dependencies import get_db, get_current_user
 from decorators.permissions import requires_role
-from schemas import business_info
-from db.models.business import BusinessInfo
+from schemas.business_info import (
+    BusinessInfoResponse,
+    BusinessInfoCreate,
+    BusinessInfoUpdate,
+)
 from db.models.user import User
-from db.crud import crud
 from utils.validators import ensure_resource_exists
+from services.business_info_service import business_info_service
 
 
 router = APIRouter(prefix="/business-info", tags=["business-info"])
 logger = logging.getLogger(__name__)
 
 
-@router.get("/", response_model=List[business_info.BusinessInfoResponse])
+@router.get("/", response_model=List[BusinessInfoResponse])
 @requires_role(["master", "client"])
 async def get_business_info(
-    user: User = Depends(verify_user), db: AsyncSession = Depends(get_db)
+    master_id: int | None = Query(None),
+    user: User = Depends(get_current_user),
+    db: AsyncSession = Depends(get_db),
 ):
-    result = await crud.read(model=BusinessInfo, session=db)
-    ensure_resource_exists(result)
-    return result
+    business_info_data = await business_info_service.get_business_info(
+        db, user, master_id
+    )
+    logger.info(f"business_info: {business_info_data}")
+
+    return business_info_data
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
 @requires_role(["master"])
 async def create_business_info(
-    business_info: business_info.BusinessInfoCreate,
+    business_info: BusinessInfoCreate,
     db: AsyncSession = Depends(get_db),
-    user: User = Depends(verify_user),
+    user: User = Depends(get_current_user),
 ):
-    master = user.masters[0]
-    logger.info(f"Business info: {business_info}")
-    result = await crud.create(
-        model=BusinessInfo,
-        session=db,
-        name=business_info.name,
-        address=business_info.address,
-        phone=business_info.phone,
-        working_hours=business_info.working_hours,
-        google_maps_link=business_info.google_maps_link,
-        description=business_info.description,
-        telegram_link=business_info.telegram_link,
-        instagram_link=business_info.instagram_link,
-        master_id=master.id
+    created_business_info = await business_info_service.create_business_info(
+        db, business_info, user.id
     )
     ensure_resource_exists(
-        result, status_code=400, message="Failed to create business info"
+        created_business_info, status_code=422, message="Failed to create business info"
     )
-    return result
+    return created_business_info
 
 
-@router.patch("/{name}/", status_code=status.HTTP_204_NO_CONTENT)
+@router.patch("/", status_code=status.HTTP_204_NO_CONTENT)
 @requires_role(["master"])
 async def update_business_info(
-    name: str,
-    business_info: business_info.BusinessInfoUpdate,
+    business_info: BusinessInfoUpdate,
     db: AsyncSession = Depends(get_db),
-    user: User = Depends(verify_user),
+    user: User = Depends(get_current_user),
 ):
-    master = user.masters[0]
-    logger.info(f"Update business info:{name}")
-    update_data = business_info.model_dump(exclude_unset=True)
-
-    result = await crud.update(
-        model=BusinessInfo,
-        session=db,
-        expressions=(BusinessInfo.master_id == master.id,),
-        **update_data,
+    result = await business_info_service.update_business_info(
+        db, user.id, business_info
     )
 
     ensure_resource_exists(result)
--- services/business_info_service.py
+++ services/business_info_service.py
@@ -2,22 +2,62 @@ import logging
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select, update
 
-from typing import List
+from typing import List, Optional, Union
 
-from schemas import business_info
+from db.models.user import User
+from schemas.business_info import BusinessInfoCreate, BusinessInfoUpdate
 from db.models.business import BusinessInfo
 from db.crud import new_crud
+from .user import user_tools, UserTools
+
 
 logger = logging.getLogger(__name__)
 
 class BusinessInfoService:
 
-    async def create_business_info(self, session: AsyncSession, business_info_data: business_info.BusinessInfoCreate, user_id: int):
-        logger.info(f"Business info: {business_info}")
-        business_info_dump = business_info_data.model_copy(update={"master_id": user_id}).model_dump()
-        query = BusinessInfo(**business_info_dump)
-        created_business_info = await new_crud.create(query, session)
-        return created_business_info
-    
+    def __init__(self, user_tools: UserTools):
+        self.user_tools = user_tools
 
-business_info_service = BusinessInfoService()
+    async def create_business_info(
+        self,
+        session: AsyncSession,
+        business_info_data: BusinessInfoCreate,
+        user_id: int,
+    ) -> Optional[BusinessInfo]:
+        logger.info(f"Business info: {business_info_data}")
+        business_info_dump = business_info_data.model_dump()
+        business_info_dump["master_id"] = user_id
+        logger.info(f"business_info_dump: {business_info_dump}")
+        stmt = BusinessInfo(**business_info_dump)
+        created_business_info = await new_crud.create(stmt, session)
+        return created_business_info
+
+    async def get_business_info(
+        self, session: AsyncSession, user: User, master_id: Union[int, None]
+    ) -> Optional[List[BusinessInfo]]:
+        filters = {}
+        if not master_id:
+            filters = await self.user_tools.identify_role(user, filters)
+        logger.info(f"filters: {filters}")
+        result = await new_crud.read(select(BusinessInfo).filter_by(**filters), session)
+        business_info = result.scalars().all()
+
+        return business_info
+
+    async def update_business_info(
+        self,
+        session: AsyncSession,
+        user_id: int,
+        updating_business_info: BusinessInfoUpdate,
+    ) -> bool:
+        update_data = updating_business_info.model_dump(exclude_unset=True)
+
+        return await new_crud.update(
+            update(BusinessInfo)
+            .where(BusinessInfo.master_id == user_id)
+            .values(**update_data),
+            session,
+        )
+
+
+business_info_service = BusinessInfoService(user_tools)
