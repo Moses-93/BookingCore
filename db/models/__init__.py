31db5a2b304665e7b41381ea6bd4f001173eb9cb
Moses-93 <moieseenko2001@gmail.com>
2025-03-13 12:34:49 +0200

    Import all models

---
+++ db/models/__init__.py
@@ -0,0 +1,7 @@
+from .booking import Time, Date, Booking
+from .business import BusinessInfo
+from .feedback import Feedback
+from .payment import Payment
+from .service import Service
+from .subscription import Subscription, SubscriptionPlan
+from .user import user_master_association, User
