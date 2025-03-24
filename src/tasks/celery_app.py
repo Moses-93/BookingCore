9be207ceda9b29ade865700b72069f6350e99fa8
Moses-93 <moieseenko2001@gmail.com>
2025-03-24 18:25:33 +0200

    Refactoring Celery config.

--- src/tasks/celery_app.py
+++ src/tasks/celery_app.py
@@ -1,35 +1,31 @@
-from celery import Celery
 from celery.schedules import crontab
 from core.config import settings
 
 
-celery_app = Celery(
-    "tasks",
-    broker=settings.redis_url,
-    include=[
+class CeleryConfig:
+    broker_url = settings.redis_url
+    result_backend = settings.redis_url
+    timezone = "Europe/Kyiv"
+    broker_connection_retry_on_startup = True
+
+    include = [
         "tasks.reminders.reminder",
         "tasks.deactivation.booking",
         "tasks.deactivation.date",
         "tasks.deactivation.time",
-    ],
-)
+    ]
 
-celery_app.conf.beat_schedule = {
-    "check-subscriptions-daily": {
-        "task": "tasks.reminders.check_subscriptions",
-        "schedule": crontab(hour=9, minute=0),
-    },
-}
+    task_routes = {
+        "tasks.reminders.reminder.*": {"queue": "reminders"},
+        "tasks.deactivation.booking.*": {"queue": "deactivate"},
+        "tasks.deactivation.date.*": {"queue": "deactivate"},
+        "tasks.deactivation.time.*": {"queue": "deactivate"},
+    }
 
-celery_app.conf.update(
-    timezone="Europe/Kyiv",
-    broker_connection_retry_on_startup=True,
-    task_routes={
-        "tasks.reminders.reminder.send_reminder": {"queue": "reminders"},
-        "tasks.deactivation.booking.deactivate_booking": {"queue": "deactivate"},
-        "tasks.deactivation.date.deactivate_date": {"queue": "deactivate"},
-        "tasks.deactivation.time.deactivate_time": {"queue": "deactivate"},
-    },
-)
-
-celery_app.autodiscover_tasks(["tasks"])
+    beat_schedule = {
+        "check-subscriptions-daily": {
+            "task": "tasks.reminders.reminder.check_subscriptions",
+            "schedule": crontab(hour=9, minute=0),
+            "options": {"queue": "reminders"},
+        },
+    }
