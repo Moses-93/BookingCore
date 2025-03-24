f82421e7d49b0a5cfe3977828e2d2714b9a0ae57
Moses-93 <moieseenko2001@gmail.com>
2025-03-24 18:36:06 +0200

    .

--- src/services/schedule/container.py
+++
@@ -1,9 +0,0 @@
-from typing import NamedTuple
-
-from services.schedule.date_service import DateScheduleService
-from services.schedule.time_service import TimeScheduleService
-
-
-class ScheduleServices(NamedTuple):
-    time_service: TimeScheduleService
-    date_service: DateScheduleService
