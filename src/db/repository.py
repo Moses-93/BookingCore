bc605a0a7e181241e44a2d3d70098635dd0fa94a
Moses-93 <moieseenko2001@gmail.com>
2025-03-29 15:28:39 +0200

    Update CRUDRepository: add docs and decorator for exception handling

--- src/db/repository.py
+++ src/db/repository.py
@@ -3,6 +3,7 @@ from typing import List, Optional, Union
 from sqlalchemy import Delete, Select, Update
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.ext.asyncio import AsyncSession
+from src.core.exceptions.database_handler import handle_db_exceptions
 
 from .interfaces import BaseCRUD
 
@@ -11,34 +12,67 @@ logger = logging.getLogger(__name__)
 
 class CRUDRepository(BaseCRUD):
 
+    @handle_db_exceptions("create")
     async def create(
-        self,
-        query: Any,
-        session: AsyncSession,
-    ):
+        self, query: DeclarativeBase, session: AsyncSession
+    ) -> DeclarativeBase:
+        """Creates a new record in the database.
+
+        Args:
+            query (DeclarativeBase): An instance of a SQLAlchemy model
+            session (AsyncSession): Asynchronous session to perform the operation
+
+        Returns:
+            DeclarativeBase: Created model object
+        """
         session.add(query)
         await session.commit()
         await session.refresh(query)
         return query
 
+    @handle_db_exceptions("read")
     async def read(
-        self,
-        query: Select,
-        session: AsyncSession,
-    ):
-        result = await session.execute(query)
-        return result
+        self, query: Select, session: AsyncSession, single: bool = False
+    ) -> Union[Optional[DeclarativeBase], List[DeclarativeBase]]:
+        """Executes a SELECT query to the database
 
-    async def update(
-        self,
-        query: Update,
-        session: AsyncSession,
-    ) -> bool:
+        Args:
+            query (Select): SQLAlchemy Select-query
+            session (AsyncSession): Asynchronous session to perform the operation
+            single (bool, optional):  if True, returns a single object (or None), otherwise - a list. Defaults to False.
+
+        Returns:
+            Union[Optional[DeclarativeBase], List[DeclarativeBase]]: One model object (single=True) or List of objects (single=False).
+        """
+        result = await session.execute(query)
+        return result.scalars().first() if single else result.scalars().all()
+
+    @handle_db_exceptions("update")
+    async def update(self, query: Update, session: AsyncSession) -> bool:
+        """Executes a UPDATE query to the database
+
+        Args:
+            query (Update): SQLAlchemy Update-query
+            session (AsyncSession): Asynchronous session to perform the operation
+
+        Returns:
+            bool: A Boolean value indicating whether changes have occurred in the database
+        """
         result = await session.execute(query)
         await session.commit()
         return result.rowcount > 0
 
-    async def delete(self, query: Delete, session: AsyncSession):
+    @handle_db_exceptions("delete")
+    async def delete(self, query: Delete, session: AsyncSession) -> bool:
+        """Executes a UPDATE query to the database
+
+        Args:
+            query (Delete): SQLAlchemy Delete-query
+            session (AsyncSession): Asynchronous session to perform the operation
+
+        Returns:
+            bool: A Boolean value indicating whether changes have occurred in the database
+        """
         result = await session.execute(query)
         await session.commit()
         return result.rowcount > 0
