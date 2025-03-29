1e8d61482afcfce4c47588c21b64e900989f3558
Moses-93 <moieseenko2001@gmail.com>
2025-03-29 11:05:04 +0200

    Add logging and typing

--- src/api/v1/endpoints/user/user_handler.py
+++ src/api/v1/endpoints/user/user_handler.py
@@ -5,13 +5,15 @@ from src.core.dependencies.auth import get_db, get_current_user
 from src.decorators.permissions import requires_role
 from src.schemas.user import UserCreate, MasterLinkRequest
 from src.db.models.user import User
+from src.services.user.user_manager import UserManager
 
 logger = logging.getLogger(__name__)
 
 
 class UserHandler:
 
-    def __init__(self, user_manager):
+    def __init__(self, user_manager: UserManager):
+        logger.info(f"Call __init__ UserHandler for an instance {self}")
         self.user_manager = user_manager
 
     @requires_role(["admin", "master", "client"])
@@ -24,8 +26,7 @@ class UserHandler:
         user_data: UserCreate,
         db: AsyncSession = Depends(get_db),
     ):
-        new_user = await self.user_manager.create_user(user_data)
-        return new_user
+        return await self.user_manager.create_user(db, user_data)
 
     async def link_master_to_user(
         self,
@@ -33,7 +34,7 @@ class UserHandler:
         user: User = Depends(get_current_user),
         db: AsyncSession = Depends(get_db),
     ):
-        await self.user_manager.add_master_to_user(user.id, master.master_chat_id)
+        await self.user_manager.add_master_to_user(db, user.id, master.master_chat_id)
 
     @requires_role(["client", "master"])
     async def delete_user(
@@ -41,4 +42,4 @@ class UserHandler:
         user: User = Depends(get_current_user),
         db: AsyncSession = Depends(get_db),
     ):
-        await self.user_manager.delete_user(user.id)
+        await self.user_manager.delete_user(db, user.id)
